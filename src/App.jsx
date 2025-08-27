0edcdfb343ffcfb5d4e4a7444f2395c07725f895
Mushfiq <mushfiq2808@gmail.com>
2025-08-27 22:15:06 +0600

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <main>
       <div>
-        <h1>Welcome to the world of GTA and GSAP!</h1>
+        <h1 className="text-3xl text-indigo-600">Welcome to the world of GTA and GSAP!</h1>
       </div>
     </main>
   )
