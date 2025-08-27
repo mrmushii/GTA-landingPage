a80c78a270f9985e44e00309291802203db13aad
Mushfiq <mushfiq2808@gmail.com>
2025-08-27 22:39:38 +0600

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
+import Hero from "./sections/Hero"
 gsap.registerPlugin(ScrollTrigger)
 const App = () => {
   return (
     <main>
-      <div>
-        <h1 className="text-3xl text-indigo-600">Welcome to the world of GTA and GSAP!</h1>
-      </div>
+      <nav>
+        <img src="/images/nav-logo.svg" className="scale-90" />
+        <img src="/images/menu.svg"  className="w-10" />
+      </nav>
+      <Hero/>
     </main>
   )
 }
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero-section'>
+      <div className='size-full mask-wrapper'>
+        <img src="/images/hero-bg.webp" className='scale-out' alt="" />
+        <img src="/images/hero-text.webp" className='title-logo fade-out' alt="" />
+        <img src="/images/watch-trailer.png" className='trailer-logo fade-out' alt="" />
+        <div className='play-img fade-out'>
+          <img src="/images/play.png" alt=" " className='w-7 ml-1' />
+        </div>
+      </div>
+      <div>
+        <img src="/images/big-hero-text.svg" alt="" />
+      </div>
+      <div className='fake-logo-wrapper'>
+        <img src="/images/big-hero-text.svg" className='overlay-logo' alt="" />
+      </div>
+    </section>
+  )
+}
+
+export default Hero
