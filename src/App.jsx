3b1b3877bb1f42bc0cd8f21bc3a666cd8226accd
Mushfiq <mushfiq2808@gmail.com>
2025-08-27 22:01:59 +0600

    new repo for gta landing page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1>Welcome to the world of GTA and GSAP!</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
